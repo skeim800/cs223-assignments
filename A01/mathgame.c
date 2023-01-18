628122cbaed4d39e52d0dc3b3dd18ec3c69d02f0
alinen <alinen@savvysine.com>
2023-01-18 11:37:40 -0500

    assignment 01

---
+++ A01/mathgame.c
@@ -0,0 +1,11 @@
+/***************************************************
+ * mathgame.c
+ * Author: 
+ * Implements a math game
+ */
+
+#include <stdio.h>
+
+int main() {
+  return 0;
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# assignments
+Assignments for CS223 Fall 2022
+
+[Assignment 1: Hello CS223](https://brynmawr-cs223-s23.github.io/website/assts/asst01.html)
+
