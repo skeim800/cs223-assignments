d83da86c4c5bdd8d307c123af313f121335889ce
alinen <alinen@savvysine.com>
2023-02-11 10:38:30 -0500

    assignment 6

---
+++ A06/decode.c
@@ -0,0 +1,18 @@
+/*----------------------------------------------
+ * Author: 
+ * Date: 
+ * Description
+ ---------------------------------------------*/
+#include <stdio.h>
+#include <stdlib.h>
+#include "read_ppm.h"
+
+int main(int argc, char** argv) {
+  if (argc != 2) {
+    printf("usage: decode <file.ppm>\n");
+    return 0;
+  }
+
+  return 0;
+}
+
