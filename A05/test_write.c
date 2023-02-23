582f9a330008fdc6cf35e89560dc455af2b242a5
Sarah Keim <skeim@goldengate.cs.brynmawr.edu>
2023-02-23 17:18:14 -0500

    completed write ppm code

--- A05/test_write.c
+++ A05/test_write.c
@@ -1,7 +1,7 @@
 /*----------------------------------------------
- * Author: 
- * Date: 
- * Description
+ * Author: Sarah Keim
+ * Date: February 22, 2023
+ * Takes pixel data and writes to new ppm file
  ---------------------------------------------*/
 
 #include <stdio.h>
@@ -11,10 +11,28 @@
 #include "write_ppm.h"
 
 int main(int argc, char** argv) {
-  int w, h;
-  struct ppm_pixel* pixels = read_ppm("feep-raw.ppm", &w, &h);
+	int w, h;
+  	struct ppm_pixel** pixels = read_ppm_2d("feep-raw.ppm", &w, &h);
 
-  // test writing the file to test.ppm, reload it, and print the contents
-  free(pixels);
-  return 0;
+  	// test writing the file to test.ppm, reload it, and print the contents
+  	write_ppm_2d("test.ppm", pixels, w, h);
+  	
+	//checks that write was completed correctly by rerunning read_ppm
+	int w2, h2;
+  	struct ppm_pixel** pixels_test = read_ppm_2d("test.ppm", &w2, &h2);
+  	for(int i = 0; i < h2; i++){
+		for(int j = 0; j < w2; j++){
+		  	printf("(%u, %u, %u)  ", pixels_test[i][j].red, pixels_test[i][j].green, pixels_test[i][j].blue);
+	  	}
+	  	printf("\n");
+  	}
+  
+ 	for(int i = 0; i < h; i++){
+	  	free(pixels[i]);
+	  	free(pixels_test[i]);
+  	}
+		
+  	free(pixels);
+  	free(pixels_test);
+  	return 0;
 }
