2868aef0dd6b81e00aa769ed365e0bfa43403fe3
Sarah Keim <skeim@goldengate.cs.brynmawr.edu>
2023-04-19 22:12:46 -0400

    mylloc_list.c complete

--- A10/mylloc_list.c
+++ A10/mylloc_list.c
@@ -7,14 +7,53 @@ struct chunk {
   int used;
   struct chunk *next;
 };
+
 struct chunk *flist = NULL;
 
 void *malloc (size_t size) {
-  // TODO: Implement malloc with a free list (See reading for details)
-  return NULL;
+  if(size == 0){
+    return NULL;
+  }
+  struct chunk* next = flist;
+  struct chunk* prev = NULL;
+
+  while(next != NULL){
+    if(next->size >= size){
+      if(prev != NULL){
+        prev->next = next->next;
+      }
+      else{
+        flist = next->next;
+        next->used = next->size - size;
+      }
+      return (void*)(next + 1);
+    }
+    else{
+      prev = next;
+      next = next->next;
+    }
+  }
+
+  void* memory = sbrk(size + sizeof(struct chunk));
+  if(memory == (void*) -1){
+    return NULL;
+  }
+  else{
+    struct chunk* cnk = (struct chunk*)memory;
+    cnk->size = size;
+    cnk->used = size;
+    return (void*)(cnk + 1);
+  }
 }
 
 void free(void *memory) {
-  // TODO: Implement malloc with a free list (See reading for details)
+  if(memory != NULL){
+    struct chunk* cnk = (struct chunk*)((struct chunk*)memory - 1);
+    cnk->next = flist;
+    flist = cnk;
+  }
   return;
 }
+
+
+
